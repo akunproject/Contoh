0cc26bff3b0cfb7db0f230e0936ef516aebdf0ef
akunproject <campusproject.acc@gmail.com>
2021-01-21 00:05:33 +0700

    Milla

--- BubbleSortProgram.java
+++ BubbleSortProgram.java
@@ -1,6 +1,6 @@
 public class BubbleSortProgram {
     public static void main(String[] args) {
-        int[] angka = {3,5,1,6,4,2};
+        int[] angka = {7,3,5,1,6,4,2};
         int i,j,k,temp;
         
         System.out.println("Sebelum diurutkan:");
@@ -28,6 +28,6 @@ public class BubbleSortProgram {
         for(i=0; i < angka.length;i++) {    
             System.out.print(angka[i]+" ");
         }
-        System.out.println("\nMillenia Saharani");
+        System.out.println("\nMilla");
     } 
 }
